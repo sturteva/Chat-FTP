c8ef90d3ce6892cd8d07a66bf61f2393dcd50df2
Andrew Sturtevant <sturteva@flip1.engr.oregonstate.edu>
2018-10-28 11:35:25 -0700

    initiateConntact working

--- Project1/chatclient.c
+++ Project1/chatclient.c
@@ -16,6 +16,37 @@
 
 void error(const char *msg) {perror(msg); exit(0);} //Error function
 
+int initiateContact(int portNumber,char* hostname){
+
+	int socketFD;
+	struct sockaddr_in serverAddress;
+	struct hostent* serverHostInfo;
+
+	//Set up server address struct
+	memset((char*) &serverAddress, '\0',sizeof(serverAddress));
+
+	serverAddress.sin_family = AF_INET;
+        serverAddress.sin_port = htons(portNumber);
+        serverHostInfo = gethostbyname(hostname);
+        if(serverHostInfo == NULL){
+                fprintf(stderr, "CLIENT: ERROR, no such host\n");
+                exit(0);
+        }
+
+        memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
+
+        //Set up the socket
+        socketFD = socket(AF_INET,SOCK_STREAM,0);
+                if(socketFD < 0) error("CLIENT:ERROR opening socket");
+
+
+        //Connect to Server
+        if(connect(socketFD,(struct sockaddr*)&serverAddress,sizeof(serverAddress)) < 0)
+                error("CLIENT: ERROR connecting");
+        
+	return socketFD;	
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -28,7 +59,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: %s hostname port\n", argv[0]); 
 		exit(0);
 	}
-
+/*
 	//Set up server address struct
 	memset((char*) &serverAddress, '\0', sizeof(serverAddress));
 
@@ -52,6 +83,10 @@ int main(int argc, char *argv[])
 	if(connect(socketFD,(struct sockaddr*)&serverAddress, 
 		sizeof(serverAddress)) < 0)
 		error("CLIENT: ERROR connecting");
+*/
+	portNumber = atoi(argv[2]); //Get the port number, convert to in
+	char* hostname = argv[1];
+	socketFD = initiateContact(portNumber,hostname);
 
 	//Get input message from user
 	printf("ENTER MESSAGE, press ENTER");

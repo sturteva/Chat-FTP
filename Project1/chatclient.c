3cec0cc6dee8d3cb87bdcf2a697fc4f0f2960502
Andrew Sturtevant <sturteva@flip1.engr.oregonstate.edu>
2018-10-28 12:01:58 -0700

    chatclient.c sendMessage() refactor

--- Project1/chatclient.c
+++ Project1/chatclient.c
@@ -47,62 +47,67 @@ int initiateContact(int portNumber,char* hostname){
 	return socketFD;	
 }
 
+void sendMessage(char handle[],int socketFD){
+
+	int charsWritten;
+	char buffer[512];
+	char message[501];
+	
+	memset(buffer, '\0',sizeof(buffer));
+	memset(message, '\0',sizeof(message));
+
+	printf("%s",handle);
+	fgets(message,sizeof(message-1),stdin);
+	message[strcspn(message,"\n")] = '\0';
+
+	//Places the handle and message into a single buffer
+	snprintf(buffer,sizeof(buffer), "%s%s",handle,message);
+
+	charsWritten = send(socketFD,buffer,strlen(buffer),0);
+
+	if(charsWritten < 0) error("CLIENT: ERROR writing to socket");
+	if(charsWritten < strlen(buffer))
+		printf("CLIENT: WARNING: Not all data written to socket!\n");	
+		
+}
+
 int main(int argc, char *argv[])
 {
 
 	int socketFD,portNumber, charsWritten, charsRead;
-	struct sockaddr_in serverAddress;
-	struct hostent* serverHostInfo;
 	char buffer[512];
+	char handle[10];
 
 	if(argc <3){
 		fprintf(stderr, "USAGE: %s hostname port\n", argv[0]); 
 		exit(0);
 	}
-/*
-	//Set up server address struct
-	memset((char*) &serverAddress, '\0', sizeof(serverAddress));
 
-	portNumber = atoi(argv[2]); //Get the port number, convert to int
-
-	serverAddress.sin_family = AF_INET; 
-	serverAddress.sin_port = htons(portNumber);
-	serverHostInfo = gethostbyname(argv[1]);
-	if(serverHostInfo == NULL){
-		fprintf(stderr, "CLIENT: ERROR, no such host\n");
-		exit(0);
-	}
-
-	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
-
-	//Set up the socket
-	socketFD = socket(AF_INET,SOCK_STREAM,0);
-	if(socketFD < 0) error("CLIENT:ERROR opening socket");
-	
-	//Connect to Server
-	if(connect(socketFD,(struct sockaddr*)&serverAddress, 
-		sizeof(serverAddress)) < 0)
-		error("CLIENT: ERROR connecting");
-*/
 	portNumber = atoi(argv[2]); //Get the port number, convert to in
 	char* hostname = argv[1];
 	socketFD = initiateContact(portNumber,hostname);
 
+	printf("Please enter your handle(No more than 9 characters): ");
+	memset(handle, '\0',sizeof(buffer));
+	fgets(handle,sizeof(handle)-1,stdin);
+	handle[strcspn(handle, "\n")] = '>'; //Removes trailing \n
+	sendMessage(handle,socketFD);
+	
+
+/*
 	//Get input message from user
-	printf("ENTER MESSAGE, press ENTER");
+	printf("ENTER MESSAGE, press ENTER\n");
 	memset(buffer, '\0',sizeof(buffer));
 	fgets(buffer, sizeof(buffer) -1, stdin);
-	buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds	
+	buffer[strcspn(buffer, "\n")] = '>'; // Remove the trailing \n that fgets adds	
 
 	//Send message to server
 	charsWritten = send(socketFD,buffer, strlen(buffer), 0);
 	if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
 
-	//NOTE TO ANDREW: CHECK CS344 programing assignemtn, probably
-	//fixed this particular problem
 	if(charsWritten < strlen(buffer))
 		printf("CLIENT: WARNING: Not all data written to socket!\n");
-
+*/
 	//Get return message from server
 	memset(buffer, '\0', sizeof(buffer)); //clear out buffer
 	charsRead = recv(socketFD, buffer,sizeof(buffer)-1,0);

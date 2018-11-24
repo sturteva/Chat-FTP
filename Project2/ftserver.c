e10a464ec860f2ae5e92bf4d1566c22cef362208
Andrew Sturtevant <sturteva@flip2.engr.oregonstate.edu>
2018-11-24 13:50:07 -0800

    Directory Listing complete, next step file transfer

--- Project2/ftserver.c
+++ Project2/ftserver.c
@@ -13,6 +13,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <dirent.h> //To do stuff with the directory
 
 void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
 
@@ -36,32 +38,86 @@ int startUp(portNumber){
 	return listenSocketFD;
 }
 
-void handleRequest(establishedConnectionFD){
+/*Pulled from Project1 for CS372*/
+int initiateContact(int portNumber, char* hostname){
+	int socketFD;
+	struct sockaddr_in serverAddress;
+	struct hostent* serverHostInfo;
+
+	//Set up server address struct
+	memset((char*) &serverAddress, '\0',sizeof(serverAddress));
+
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(portNumber);
+        serverHostInfo = gethostbyname(hostname);
+        if(serverHostInfo == NULL){
+		fprintf(stderr, "CLIENT: ERROR, no such host\n");
+                exit(0);
+       }
+
+       memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
+
+	//Set up the socket
+	socketFD = socket(AF_INET,SOCK_STREAM,0);
+	if(socketFD < 0) error("CLIENT:ERROR opening socket");
+	//Connect to Server
+	if(connect(socketFD,(struct sockaddr*)&serverAddress,sizeof(serverAddress)) < 0)
+	error("CLIENT: ERROR connecting");
+	                                                                                                       
+		return socketFD;
+
+}
+void handleRequest(int establishedConnectionFD,int connectionPort, char client[]){
 	
-	char buffer[1024];
+	char commandBuffer[1024];
+	char dirBuffer[70000];
 	int charsRead;
+	int dataPort;
+	int dataFD;
+	DIR *d;
+	struct dirent *dir;
 	
-	memset(buffer, '\0',sizeof(buffer));
-	charsRead = recv(establishedConnectionFD, buffer, sizeof(buffer)-1,0);
+
+	memset(dirBuffer,'\0',sizeof(dirBuffer));
+	memset(commandBuffer, '\0',sizeof(commandBuffer));
+	charsRead = recv(establishedConnectionFD, commandBuffer, sizeof(commandBuffer)-1,0);
 
 	if(charsRead < 0) error("ERROR reading from socket");
 
 	//Test line
-	printf("SERVER from client: \"%s\"\n", buffer);
+	//printf("SERVER from client: \"%s\"\n", buffer);
 
-	char* token = strtok(buffer," " );
-	printf("BOO\n");
+	char* token = strtok(commandBuffer," " );
+
 	printf("token: %s\n",token);
-	if(strcmp(token,"-g") != 0 && strcmp(token,"-l") != 0){
-		printf("Invalid command sent\n");
-	}
+	if(strcmp(token,"-l") == 0){
+		dataPort = atoi(strtok(NULL," "));
+		printf("List directory requested on port %d.\n",dataPort);
+		dataFD = initiateContact(dataPort,(char*)client);
 
-	else if(strcmp(token,"-l") == 0){
-		printf("Listing Command Received");
+		d = opendir(".");
+
+		if(d){
+			while((dir = readdir(d)) != NULL){
+				strcat(dirBuffer,dir->d_name);
+				strcat(dirBuffer,"\n");	
+			}
+			closedir(d);
+		}
+		int charsWritten = 0;
+
+		while(charsWritten < strlen(dirBuffer)){
+			charsWritten+= send(dataFD,dirBuffer,strlen(dirBuffer),0);
+
+			if(charsWritten < 0)
+				error("SERVER: Error Sending Directory");
+		}	
+		
+			
 	}
 
 	else if(strcmp(token,"-g") == 0){
-		printf("Get command Received");
+		printf("Get command Received\n");
 	}
 }
 int main(int argc, char *argv[])
@@ -92,7 +148,7 @@ int main(int argc, char *argv[])
 
 	getnameinfo(&clientAddress,sizeof(clientAddress),host,sizeof(host),service,sizeof(service),0); 
 	printf("Connection from %s\n",host);
-	handleRequest(establishedConnectionFD);
+	handleRequest(establishedConnectionFD,portNumber,host);
 
 	/* Send a Success message back to the client*/
 	charsRead = send(establishedConnectionFD, "I am the server, and I got your message", 39, 0); // Send success back

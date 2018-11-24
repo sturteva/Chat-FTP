844834c44a91ff905d88af99154efe22b7b5bfb1
Andrew Sturtevant <sturteva@flip2.engr.oregonstate.edu>
2018-11-23 16:16:37 -0800

    next step is to start dataPort on Server side

--- Project2/ftserver.c
+++ Project2/ftserver.c
@@ -15,6 +15,55 @@
 #include <netinet/in.h>
 
 void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
+
+int startUp(portNumber){
+	struct sockaddr_in serverAddress;
+	int listenSocketFD;
+
+	memset((char *) &serverAddress, '\0', sizeof(serverAddress));//Clear out address struct
+	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); //Store the port number
+	serverAddress.sin_addr.s_addr = INADDR_ANY; //Any address is allowed for connection
+
+	/* Set up the socket*/
+	listenSocketFD = socket(AF_INET,SOCK_STREAM,0); //Creates the socket
+	if (listenSocketFD < 0) error("ERROR opening socket");
+
+	/*Enable the socket to begin listening*/
+	if(bind(listenSocketFD,(struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)error("ERROR on binding");
+
+	printf("Server open on %d\n", portNumber);
+	return listenSocketFD;
+}
+
+void handleRequest(establishedConnectionFD){
+	
+	char buffer[1024];
+	int charsRead;
+	
+	memset(buffer, '\0',sizeof(buffer));
+	charsRead = recv(establishedConnectionFD, buffer, sizeof(buffer)-1,0);
+
+	if(charsRead < 0) error("ERROR reading from socket");
+
+	//Test line
+	printf("SERVER from client: \"%s\"\n", buffer);
+
+	char* token = strtok(buffer," " );
+	printf("BOO\n");
+	printf("token: %s\n",token);
+	if(strcmp(token,"-g") != 0 && strcmp(token,"-l") != 0){
+		printf("Invalid command sent\n");
+	}
+
+	else if(strcmp(token,"-l") == 0){
+		printf("Listing Command Received");
+	}
+
+	else if(strcmp(token,"-g") == 0){
+		printf("Get command Received");
+	}
+}
 int main(int argc, char *argv[])
 {
 	int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
@@ -25,29 +74,13 @@ int main(int argc, char *argv[])
 	/*START OF THE SERVER START-UP*/
 	if (argc < 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
 
-	/* Set up the address struct for this process (the server)*/
-	memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+
 	portNumber = atoi(argv[1]); // Get the port number, convert to an integer from a string
-	serverAddress.sin_family = AF_INET; // Create a network-capable socket
-	serverAddress.sin_port = htons(portNumber); // Store the port number
-	serverAddress.sin_addr.s_addr = INADDR_ANY; // Any address is allowed for connection to this process
-/* Set up the socket*/
-		
-	listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
-	if (listenSocketFD < 0) error("ERROR opening socket");
-
-	/* Enable the socket to begin listening*/
-	if (bind(listenSocketFD, (struct sockaddr *)&serverAddress,sizeof(serverAddress)) < 0) // Connect socket to port
-			error("ERROR on binding");
-
-	printf("Server open on %d\n",portNumber);
+	listenSocketFD = startUp(portNumber);
 	listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections
-
 	
-	/*END OF START-UP*/
-
 	/* Accept a connection, blocking if one is not available until one connects*/
-
+	while(1){
 	sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
 
 	establishedConnectionFD = accept(listenSocketFD,(struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
@@ -59,20 +92,15 @@ int main(int argc, char *argv[])
 
 	getnameinfo(&clientAddress,sizeof(clientAddress),host,sizeof(host),service,sizeof(service),0); 
 	printf("Connection from %s\n",host);
-
-	/* Get the message from the client and display it*/
-	memset(buffer, '\0', 256);
-	charsRead = recv(establishedConnectionFD, buffer, 255, 0); // Read the client's message from the socket
-
-	if (charsRead < 0) error("ERROR reading from socket");
-
-	printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+	handleRequest(establishedConnectionFD);
 
 	/* Send a Success message back to the client*/
 	charsRead = send(establishedConnectionFD, "I am the server, and I got your message", 39, 0); // Send success back
 	if (charsRead < 0) error("ERROR writing to socket");
 
 	close(establishedConnectionFD); // Close the existing socket which is connected to the client
+	}
+
 	close(listenSocketFD); // Close the listening socket
 
 	return 0;
